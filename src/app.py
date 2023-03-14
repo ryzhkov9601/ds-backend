7bffe442dcae777e12b7235054e5dcd2943e66b2
Alexander Ryzhkov <ryzhkov9601@gmail.com>
2023-03-14 16:11:58 +0000

    solve homework

--- src/app.py
+++ src/app.py
@@ -3,38 +3,65 @@ from models.plate_reader import PlateReader
 import logging
 from PIL import UnidentifiedImageError
 import io
+import requests
+from plate_client import PlateClient, MyException
+
 
 app = Flask(__name__)
 plate_reader = PlateReader.load_from_file('./model_weights/plate_reader_model.pth')
+plate_client = PlateClient()
+
+
+def read_data(data):
+    im = io.BytesIO(data)
+    try:
+        result = plate_reader.read_text(im)
+    except UnidentifiedImageError:
+        return {'error': 'invalid image'}, 400
+    return {'name': result}
 
 
 @app.route('/')
 def hello():
-    return '<h1><center>Hello w!</center></h1>'
+    return '<h1><center>Hello user!</center></h1>'
 
-
-@app.route('/toUpper')
-def to_upper():
-    if 's' not in request.args:
-        return 'invalid args', 400
-    s = request.args.get('s', 'missing')
-    return {
-        'result': s.upper()
-    }
 
 # <url>:8080/readNumber : body <image bytes>
 # {"name": "o123e11"}
 @app.route('/readNumber', methods=["POST"])
 def read_number():
     body = request.get_data()
-    im = io.BytesIO(body)
+    return read_data(body)
+
+
+# <url>:8080/readNumberById?id=10022
+# <url>:8080 : body: {"id": "10022"}
+# -> {"name": "o123e11"}
+@app.route('/readNumberById')
+def read_remote_image():
+    if 'id' not in request.args:
+        return {'error': 'invalid args'}, 400
+    image_id = request.args.get('id')
     try:
-        result = plate_reader.read_text(im)
-    except UnidentifiedImageError:
-        return {'error': 'invalid image'}, 400
-    return {
-        'name': result
-    }
+        result = plate_client.read_number_by_id(image_id)
+    except MyException as err:
+        return err.message, err.status_code
+    return result
+
+
+# <url>:8080/readArray?id=10022&id=9965
+# -> {"10022": "с181мв190", "9965": "о101но750"}
+@app.route('/readArray')
+def read_array():
+    if 'id' not in request.args:
+        return {'error': 'invalid args'}, 400
+    arr = request.args.getlist('id')
+    try:
+        result = plate_client.read_array(arr)
+    except MyException as err:
+        return err.message, err.status_code
+    return {image_id:res for image_id, res in zip(arr, result)}
+
 
 if __name__ == '__main__':
     logging.basicConfig(
--- src/plate_client.py
+++ src/plate_client.py
@@ -1,24 +1,61 @@
 import requests
 
-class PlateClient:
-    def __init__(self, url: str):
-        self.url = url
 
-    def read_number(self, im) -> str:
+class MyException(Exception):
+    def __init__(self, message, status_code):
+        self.message = {'error': message}
+        self.status_code = status_code
+
+    def __str__(self):
+        return self.message
+
+
+class PlateClient:
+    def __init__(self, url = 'http://127.0.0.1:8080', image_storage_url='http://51.250.83.169:7878/images'):
+        self.url = url
+        self.image_storage_url = image_storage_url
+        self.valid_ids = [10022, 9965]
+
+    def read_number(self, im):
         res = requests.post(
             f'{self.url}/readNumber',
             headers={'Content-Type': 'application/x-www-form-urlencoded'},
             data=im,
             timeout=1
         )
-        return res.json()['name']
+        return res.json()
     
-    def upper(self):
-        pass
+    def read_number_by_id(self, image_id):
+        if int(image_id) not in self.valid_ids:
+            raise MyException('invalid id of image', 400)
+        try:
+            response = requests.get(
+                f'{self.image_storage_url}/{image_id}',
+                timeout=1
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            raise MyException('image not found', 400)
+        except requests.exceptions.ConnectionError:
+            raise MyException('can not connect', 500)
+        except requests.exceptions.Timeout:
+            raise MyException('request timeout', 500)
+        except requests.exceptions.RequestException as err:
+            raise MyException(str(err), 500)
+        return self.read_number(response.content)
+
+    def read_array(self, arr: list):
+        return [self.read_number_by_id(image_id)['name'] for image_id in arr]
 
 
 if __name__ == '__main__':
     client = PlateClient('http://127.0.0.1:8080')
     with open('images/10022.jpg', 'rb') as im:
         res = client.read_number(im)
+    print(res)
+
+    res = client.read_number_by_id(10022)
+    print(res)
+
+    res = client.read_array([10022, 9965])
     print(res)
